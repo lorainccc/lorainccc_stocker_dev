5722c31aa2169c2cf373551dc94ca39c5c071263
David Michael Brattoli <dbrattoli@lorainccc.edu>
2017-03-29 11:56:37 -0400

    updated taxonomy archive

--- taxonomy-event_categories.php
+++ taxonomy-event_categories.php
@@ -40,26 +40,29 @@ get_header(); ?>
 <?php
 			// get the currently queried taxonomy term, for use later in the template file
 $term = get_queried_object();
-			$args = array(
+					$paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+     $args = array(
     'post_type' => 'lccc_events',
     'event_categories' => $term->slug,
     'post_status' => 'publish',
     'order'=> 'ASC',
     'orderby'=> 'meta_value',
+				'paged' => $paged,		
     'meta_key' => 'event_start_date',
 );
 $query = new WP_Query( $args );
-  // output the term name in a heading tag
-    echo '<h2>' . $term->name . ' Events </h2>';
 
-				// output the term descriptopn in a paragraph tag
-     echo '<p>' . $term->description . '</p>';
+    // output the term name in a heading tag
+    	echo '<h2>' . $term->name . ' Events </h2>';
 
-			// output the link to the page that contains the Category Description
-			$siteurl= get_site_url();
-			echo '<a href="'.$siteurl.'/' . $term->slug .'">'. 'Learn More</a>';
-	
-		if ($query->have_posts()):
+    // output the term descriptopn in a paragraph tag
+     	echo '<p>' . $term->description . '</p>';
+
+    // output the link to the page that contains the Category Description
+	$siteurl= get_site_url();
+	echo '<a href="'.$siteurl.'/' . $term->slug .'">'. 'Learn More</a>';
+
+if ($query->have_posts()):
         // Start the Loop
         while ( $query->have_posts() ) : $query->the_post();
 
@@ -113,20 +116,17 @@ $location = event_meta_box_get_meta('event_meta_box_event_location');
 $cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="small-12 medium-2 large-2 columns">
+		<?php 
+if ( has_post_thumbnail() ) { ?>
+		<div class="small-12 medium-12 large-4 columns" style=" padding-left: 0;padding-right: 0.6rem;margin-top: 0.8rem;">
 	<?php
-			echo '<div class="small-12 medium-12 large-12 columns event-date">';
-         echo '<div class="small-12 medium-12 large-12 columns calendar">';
-										echo '<p class="stocker-month">'.$eventstartmonth.'</p>';
-										echo '<p class="stocker-day">'.$eventstartday.'</p>';
-						echo '</div>';
-			echo '</div>';
+							the_post_thumbnail();
 		?>
  </div>
-	<div class="small-12 medium-10 large-10 columns nopadding">
+	<div class="small-12 medium-10 large-8 columns nopadding">
 	<div class="small-12 medium-12 large-12 columns nopadding">
 		<header class="entry-header">
-        <a href="<?php the_permalink();?>"><?php the_title( '<h2 class="entry-title">', '</h2>' ); ?></a>
+        <a href="<?php the_permalink();?>"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
        <div class="taxonomies">
 	<?php echo get_the_term_list( $post->ID, 'event_categories', '', ' , ' , ''); ?>
 </div>
@@ -167,17 +167,72 @@ $cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');
 			?>
 	<?php endif; ?>
 </div>
+	
+	
+	<?php }else{ ?>
+	<div class="small-12 medium-10 large-12 columns nopadding">
+	<div class="small-12 medium-12 large-12 columns nopadding">
+		<header class="entry-header">
+        <a href="<?php the_permalink();?>"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
+       <div class="taxonomies">
+	<?php echo get_the_term_list( $post->ID, 'event_categories', '', ' , ' , ''); ?>
+</div>
+        <p><?php echo 'Date: '.$eventstartmonthfull.' '.$eventstartday.', '.$eventstartyear; ?></p>
+        <p><?php echo 'Time: '.$starttime; ?></p>
+          <p><?php echo 'Location: '.$location; ?></p>
+        <p><?php echo 'Cost: '.$cost; ?></p>
+        <p>&nbsp;</p>
+
+	</header><!-- .entry-header -->
+	</div>
+	<div class="small-12 medium-12 large-12 columns nopadding">
+	<div class="entry-content">
+		<?php
+			the_excerpt();
+		?>
+	<a href="<?php the_permalink();?>">More Information</a>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'lorainccc' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	</div>
+	<?php if ( get_edit_post_link() ) : ?>
+
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'lorainccc' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+	<?php endif; ?>
+</div>
+	<?php } ?>
 	</article><!-- #post-## -->
 <div class="column row event-list-row">
     <hr>
   </div>
 
-        <?php endwhile;
-
+        <?php endwhile; ?>
+<div id="pagination" class="clearfix">
+  <div style="float:left;"><?php previous_posts_link( 'Previous Events' ); ?></div>
+  <div style="float:right;"><?php next_posts_link( 'More Events', $wp_query->max_num_pages ); ?></div>
+</div>			
+			
+			<?php
+			// use reset postdata to restore orginal query
+			   wp_reset_postdata();
 endif; // end of check for query having posts
 
-// use reset postdata to restore orginal query
-wp_reset_postdata();
+
+
 
 			?>
 
